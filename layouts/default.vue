6825eed4c81a850c516efb71768caf3d2ac83dba
YJack0000 <jackleagends@gmail.com>
2023-08-20 16:45:08 +0800

    adding payout for landing and default

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Header />
+        <div class="min-h-screen max-w-6xl mx-auto flex flex-col">
+            <NuxtPage class="flex-1 p-4 xl:p-0" />
+        </div>
+        <Footer />
+    </div>
+</template>
---
+++ layouts/landing.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex min-h-screen flex-col">
+        <NuxtPage class="flex-1 p-0" />
+        <Footer />
+    </div>
+</template>
