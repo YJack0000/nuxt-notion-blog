4f4f1a26649588d1b80e2afe9b33408447dab0a6
YJack0000 <jackleagends@gmail.com>
2023-08-20 19:53:02 +0800

    feat: setup notion custom css for blog and import through nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,13 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
         '@nuxtjs/tailwindcss',
+        '@vueuse/nuxt'
         // 'vue-notion/nuxt'
     ],
     build: {
         transpile: ['vue-notion'],
     },
-    rutimeConfig: {
+    runtimeConfig: {
         NOTION_POST_DATABASE_ID: process.env.NOTION_POST_DATABASE_ID,
         NOTION_API_KEY: process.env.NOTION_API_KEY,
         public: {
@@ -16,20 +17,23 @@ export default defineNuxtConfig({
             LINKEDIN_PAGE: 'https://www.linkedin.com/in/宇傑-鄭-3941181a3',
         },
     },
-    head: {
-        htmlAttrs: {
-            lang: 'zh',
-        },
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-        charset: 'utf-8',
-        viewport: 'width=device-width, initial-scale=1',
-        title: "YJack's Site.",
-        meta: [
-            {
-                name: 'description',
-                content:
-                    '這是一個用 Nuxt.js、Tailwindcss、Notion 作為資料庫、Vercel 作為部署平台的個人網站。用來存放 YJack0000 的文章與個人資料。',
+    css: ['@/assets/styles/vue-notion-dark.css'],
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'zh',
             },
-        ],
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: "YJack's Site.",
+            meta: [
+                {
+                    name: 'description',
+                    content:
+                        '這是一個用 Nuxt.js、Tailwindcss、Notion 作為資料庫、Vercel 作為部署平台的個人網站。用來存放 YJack0000 的文章與個人資料。',
+                },
+            ],
+        },
     },
 })
