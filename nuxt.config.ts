24fb5d9df5dda52621361dc42f1423ef7b9bbd35
YJack0000 <jackleagends@gmail.com>
2023-08-20 16:45:49 +0800

    feat: setup notion vaiables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        // 'vue-notion/nuxt'
+    ],
+    build: {
+        transpile: ['vue-notion'],
+    },
+    rutimeConfig: {
+        NOTION_POST_DATABASE_ID: process.env.NOTION_POST_DATABASE_ID,
+        NOTION_API_KEY: process.env.NOTION_API_KEY,
+        public: {
+            BLOG_NAME: "YJack's blog",
+            GITHUB_PAGE: 'https://github.com/YJack0000',
+            LINKEDIN_PAGE: 'https://www.linkedin.com/in/宇傑-鄭-3941181a3',
+        },
+    },
+    head: {
+        htmlAttrs: {
+            lang: 'zh',
+        },
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+        title: "YJack's Site.",
+        meta: [
+            {
+                name: 'description',
+                content:
+                    '這是一個用 Nuxt.js、Tailwindcss、Notion 作為資料庫、Vercel 作為部署平台的個人網站。用來存放 YJack0000 的文章與個人資料。',
+            },
+        ],
+    },
 })
